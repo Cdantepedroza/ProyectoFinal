bdf80db846da75a952e2d994811e6cb6f3bfbf03
Cdantepedroza <christiandantepi@gmail.com>
2023-11-09 12:41:48 -0500

    menu

---
+++ src/main/java/org/modelo/Pedido.java
@@ -0,0 +1,2 @@
+package org.modelo;public class Pedido {
+}
--- src/main/java/org/vista/Main.java
+++
@@ -1,8 +0,0 @@
-package org.vista;
-
-public class Main {
-    public static void main(String[] args){
-        System.out.println("prueba commit");
-        System.out.println("prueba commit2");
-    }
-}
